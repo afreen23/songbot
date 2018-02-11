3b4ffcda72d2a26ce43d8357b7525a3d3a8719e4
afreen24 <studentinsha12@gmail.com>
2018-02-11 22:02:04 +0530

    changed cluster

--- microservices/ui/app/src/index.js
+++ microservices/ui/app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Chat from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Chat/>, document.getElementById('root'));
 registerServiceWorker();
