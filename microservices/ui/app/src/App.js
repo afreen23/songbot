118c0751891ccda16b931fad02ce7cd95d9346d1
afreen24 <studentinsha12@gmail.com>
2018-02-15 17:53:24 +0530

    endpoint fix

--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -56,7 +56,7 @@ constructor(props){
   }
 //fetching bot's greeting message
 componentDidMount() {
- fetch('https://ui.aesthete80.hasura-app.io/input', {
+ fetch('https://app.aesthete80.hasura-app.io/input', {
     method: 'POST',
     body: JSON.stringify({
       input: 'hi'
@@ -85,7 +85,7 @@ handleSubmit(e) {
   currentHistory = currentHistory.concat({type:'bot',loading: true});
   this.setState({chatHistory: currentHistory });
    //for sending reply
-   fetch('https://ui.aesthete80.hasura-app.io/input', {
+   fetch('https://app.aesthete80.hasura-app.io/input', {
     method: 'POST',
     body: JSON.stringify({
       input: e
